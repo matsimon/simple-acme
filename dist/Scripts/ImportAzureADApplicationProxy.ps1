3a485080ea494b9841ee03464a62aaff1c9c48b8
Mathieu Simon <mathieu.simon@simweb.ch>
2026-06-03 16:21:20 +0200

    Update ImportAzureADApplicationProxy.ps1

--- dist/Scripts/ImportAzureADApplicationProxy.ps1
+++ dist/Scripts/ImportAzureADApplicationProxy.ps1
@@ -29,9 +29,13 @@ The application (client) ID of the app registration used to authenticate. The ap
 .PARAMETER ClientSecret
 The client secret of the app registration.
 
+.PARAMETER TargetAppId
+The Application (client) ID of the Enterprise Application whose certificate should be updated.
+Find this value in Entra ID > Enterprise Applications > <your app> > Application ID.
+
 .EXAMPLE
 
-ImportAzureApplicationProxy.ps1 <PfxPath> <CertPass> <TenantId> <ClientId> <ClientSecret>
+ImportAzureApplicationProxy.ps1 <PfxPath> <CertPass> <TenantId> <ClientId> <ClientSecret> <TargetAppId>
 
 .NOTES
 Authentication uses the OAuth 2.0 client credentials flow via Connect-MgGraph -ClientSecretCredential,
@@ -41,21 +45,19 @@ which requires Microsoft.Graph module v2.0 or later:
 
 #>
 
+#Requires -Version 7.4
+#Requires -Modules @{ ModuleName = "Microsoft.Graph.Authentication";    ModuleVersion = "2.0"  }
+#Requires -Modules @{ ModuleName = "Microsoft.Graph.Applications";      ModuleVersion = "2.0"  }
+#Requires -Modules @{ ModuleName = "Microsoft.Graph.Beta.Applications"; ModuleVersion = "2.10" }
+
 param(
     [Parameter(Position=0,Mandatory=$false)][string]$PfxPath,
     [Parameter(Position=1,Mandatory=$true)][string]$CertPass,
     [Parameter(Position=2,Mandatory=$true)][string]$TenantId,
     [Parameter(Position=3,Mandatory=$true)][string]$ClientId,
-    [Parameter(Position=4,Mandatory=$true)][string]$ClientSecret
+    [Parameter(Position=4,Mandatory=$true)][string]$ClientSecret,
+    [Parameter(Position=5,Mandatory=$true)][string]$TargetAppId
 )
-
-# Convert the password for the certificate to a secure string
-$SecureCertPass = ConvertTo-SecureString -String $CertPass -AsPlainText -Force
-
-
-if (!(Get-Module -ListAvailable -Name Microsoft.Graph)) {
-    Throw "Missing Microsoft.Graph module, install with 'Install-Module -Name Microsoft.Graph -Scope AllUsers'"
-}
 
 # Connect to Microsoft Graph using client credentials (application secret)
 $SecureSecret = ConvertTo-SecureString -String $ClientSecret -AsPlainText -Force
@@ -63,49 +65,29 @@ $ClientSecretCredential = New-Object -TypeName System.Management.Automation.PSCr
 $null = Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $ClientSecretCredential -NoWelcome
 
 
-# It's easier, apparently, to search for the service principals that are tagged with WindowsAzureActiveDirectoryOnPremApp,
-# then match them to the Get-AzureADApplication output by AppId.
-# Get-AzureADApplication doesn't have any way to filter only for ones using the application proxy, and 
-# Get-AzureADApplicationProxyApplication requires an ObjectId, there's no way to just list them all.
-$aadapServPrinc = Get-MgServicePrincipal -Property Displayname,Id,Appid,Tags -All | where-object {$_.Tags -Contains "WindowsAzureActiveDirectoryOnPremApp"}
+$app = Get-MgApplication -Filter "AppId eq '$TargetAppId'" -ErrorAction Stop
+if (-not $app) { Throw "No application found with Application ID '$TargetAppId'." }
+"Targeting application: $($app.DisplayName)"
 
-# Now we get a list of all Azure AD Applications
-$aadapps = Get-MgApplication -All | Sort-Object -Property AppId
+$appDetail = Get-MgBetaApplication -ApplicationId $app.Id -Select DisplayName,AppId,Id,OnPremisesPublishing
+Write-Host "External URL : $($appDetail.OnPremisesPublishing.ExternalUrl)"
+Write-Host "Current certificate:"
+$appDetail.OnPremisesPublishing.VerifiedCustomDomainCertificatesMetadata
 
-# The AppId between $aadapServPrinc and $aadapps is the same for each of the applications using Azure AD Application Proxy.
-# What we need to get is the ObjectId from $aadapps for each application that was in $aadapServPrinc
-$aadProxyApps = New-Object System.Collections.ArrayList
-#$aadproxyapps = $aadapServPrinc | Foreach-Object { $aadapps -match $_.AppId}
-foreach ($Principal in $aadapServPrinc){
-    write-host $Principal.AppId
-    foreach($App in $aadapps){
-       if($Principal.AppId -eq $App.AppId){
-            $aadProxyApps.add($App)
+$certParams = @{
+    onPremisesPublishing = @{
+        verifiedCustomDomainKeyCredential = @{
+            type  = "X509CertAndPassword"
+            value = [convert]::ToBase64String([System.IO.File]::ReadAllBytes($PfxPath))
+        }
+        verifiedCustomDomainPasswordCredential = @{
+            value = $CertPass
         }
     }
 }
-# Now $aadaproxyapps has just the Get-MgApplication objects for applications that use the Azure AD Application Proxy.
 
-"Found $($aadproxyapps.count) applications to update"
-
-# Get the matching objects from Get-AzureADApplicationProxyApplication and show the certificate being used
-#$aadproxyapps | Foreach-Object { 
-foreach($proxyApp in $aadproxyapps){
-    $proxyapp = Get-MgBetaApplication -ApplicationId $ProxyApp.Id -Select DisplayName,AppId,Id,OnPremisesPublishing
-    Write-Host "Checking $($proxyapp.OnPremisesPublishing.ExternalUrl)"
-    Write-Host "Existing certificate is:"
-    $($proxyapp.OnPremisesPublishing.VerifiedCustomDomainCertificatesMetadata)
-#    
-}
-
-# The documentation says "If you have one certificate that includes many of your applications, you only need to upload it with one application and it will also be assigned to the other relevant applications."
-# That does not seem to be the case. Updating the certificate for one application only updated that single application, the rest keep using the old certificate.
-# Perhaps it just takes a bit to update, but I thought it safer to just update all of them.
-
-$aadproxyapps | Foreach-Object {
-    "Updating certificate for $($_.DisplayName)"
-    Set-AzureADApplicationProxyApplicationCustomDomainCertificate -ObjectId $_.ObjectId -PfxFilePath $PfxPath -Password $SecureCertPass
-}
+"Updating certificate for $($app.DisplayName)"
+Update-MgBetaApplication -ApplicationId $app.Id -BodyParameter $certParams
 
 
 Disconnect-MgGraph
